7a5ad50d12211e7a7d756b2f74cd2239821ad883
John Wästerlund <grunka@gmail.com>
2018-07-04 16:55:13 +0200

    Using a custom event to get callback into the correct context for firefox

--- src/nolimit-api.js
+++ src/nolimit-api.js
@@ -32,7 +32,10 @@ var nolimitApiFactory = function(target, onload) {
 
     if(target.nodeName === 'IFRAME') {
         if (target.contentWindow) {
-            addMessageListener(target.contentWindow);
+            target.addEventListener('nolimit-load', function() {
+                addMessageListener(target.contentWindow);
+            });
+            target.dispatchEvent(new CustomEvent('nolimit-load'));
         } else {
             target.addEventListener('load', function() {
                 addMessageListener(target.contentWindow);
