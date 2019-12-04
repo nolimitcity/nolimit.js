0cac953b3f8cfc84ce97fbf7500481a5af5be647
John Wästerlund <grunka@gmail.com>
2019-12-04 10:57:27 +0100

    Using currency as play for fun currency if supplied

--- src/nolimit.js
+++ src/nolimit.js
@@ -304,6 +304,7 @@ function processOptions(options) {
     }
     options.cdn = options.cdn || CDN.replace('{ENV}', environment);
     options.staticRoot = options.staticRoot || GAMES_URL.replace('{CDN}', options.cdn);
+    options.playForFunCurrency = options.playForFunCurrency || options.currency;
     return options;
 }
 
